6f7cd2f9ec91ffc377fd8c50e61c09d19082a97a
Timothy Soladoye <timothy@snapnet.com.ng>
2020-11-18 09:46:38 +0100

    add name to permissioncategory fillable

--- app/PermissionCategory.php
+++ app/PermissionCategory.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class PermissionCategory extends Model
 {
     //
+    protected $fillable = ['name'];
     public function permissions()
     {
         return $this->hasMany('App\Permission');
